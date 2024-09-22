6922eab19cc366b3614a515857b4c6a61e85b432
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-09-22 19:03:28 -0300

    consertado problema que baixava o html inves de abrilo no localhost

--- index4.js
+++ index4.js
@@ -1,13 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const app = express();
 const port = 3000;
 var bandName = "";
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,8 +23,11 @@ function bandNameGenerator(req, res, next) {
 app.use(bandNameGenerator);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/test.html");
+  res.setHeader('Content-Type', 'text/html'); 
+  res.removeHeader('Content-Disposition'); 
+  res.sendFile(path.join(__dirname, "public", "test.html"));
 });
+
 
 app.post("/submit", (req, res) => {
   res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
