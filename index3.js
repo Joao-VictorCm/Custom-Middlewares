419390326b6e8ea9128d26ff22a764cc6ca8e8be
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-09-21 22:43:42 -0300

    Compreendendo melhor os custon middleware

--- index3.js
+++ index3.js
@@ -3,10 +3,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+function logger(req, res, next) {
+  console.log("Request method:", req.method)
+  console.log("Request url:", req.url)
+  next()
+}
+
+function data(req, res, next){
+  const d = new Date()
+  console.log(d)
+  next()
+}
+
 app.use(logger);
 
+app.use(data)
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello" + data.d);
 });
 
 app.listen(port, () => {
